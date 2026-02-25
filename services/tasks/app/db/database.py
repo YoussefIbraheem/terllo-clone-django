d0b3a2472ced556ffe5b1b61e22dc88d32f017e8
YoussefIbraheem <youssefm.muhammed@gmail.com>
2026-02-25 02:24:19 +0200

    Refactor database session management: enhance context manager usage and enable SQL echo for debugging

--- services/tasks/app/db/database.py
+++ services/tasks/app/db/database.py
@@ -1,38 +1,37 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session, sessionmaker
+from contextlib import contextmanager
 from app import settings
 from app.models import Base
 from typing import Iterator
 
 
-engine = create_engine(settings.DB_URL, pool_pre_ping=True)
+engine = create_engine(settings.DB_URL, pool_pre_ping=True, echo=True)
 
-sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
-
+@contextmanager
 def get_db_session() -> Iterator[Session]:
-    db = sessionLocal()
-    
-    try:
-        yield db
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        raise Exception(f"Transaction Failed: {e}")
-    finally:
-        db.close()
-    
-    
+    with SessionLocal() as db:
+
+        try:
+            yield db
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            raise Exception(f"Transaction Failed: {e}")
+        finally:
+            db.close()
 
 
 def get_db():
-    db = sessionLocal()
+    with SessionLocal() as db:
 
-    try:
-        yield db
-    finally:
-        db.close()
+        try:
+            yield db
+        finally:
+            db.close()
