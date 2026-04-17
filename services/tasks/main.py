de061cb3a4dd46ae18696d328a6b18950588a81d
YoussefIbraheem <youssefm.muhammed@gmail.com>
2026-04-17 04:25:13 +0200

    Add OpenAPI documentation generation and update requirements.txt

--- services/tasks/main.py
+++ services/tasks/main.py
@@ -1,5 +1,6 @@
-from flask_migrate import Migrate
 from app import settings, create_app
+from utils import generate_open_api_doc
+from swagger_ui import api_doc
 
 app = create_app()
 
@@ -10,6 +11,14 @@ def index():
 
 
 if __name__ == "__main__":
+
+   
+    generate_open_api_doc(app=app)
+    api_doc(app,config_path="./openapi.yaml",url_prefix="/api/swagger",title="API Doc")
+    
+    
+    
+    
 
     print(
         f"Starting {settings.SERVICE_NAME} service (version {settings.SERVICE_VERSION}) on {settings.HOST}:{settings.PORT} with debug={settings.DEBUG}"
